62d876365dcb7933a6ecb05cdaec0eee36e0b9a5
ShareYourScript <mat@shareyourscript.me>
2019-07-30 09:47:02 +0200

    fix eb-cli

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 set -e
+set -x
 
-ln -s /root/.local/bin/* /usr/local/bin/
+AWS_CONFIG_FILE="/root/.aws/config
 
 if [[ -n ${AWS_ACCESS_KEY_ID} ]] && [[ -n ${AWS_SECRET_ACCESS_KEY} ]] && [[ -n ${AWS_DEFAULT_REGION} ]]; then
   mkdir -p /root/.aws
-  echo -e "[profile eb-cli]\naws_access_key_id = ${AWS_ACCESS_KEY_ID}\naws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}\n\n[default]\noutput = json\nregion = ${AWS_DEFAULT_REGION}" > /root/.aws/config \
-  && chmod 600 /root/.aws/config;
+  echo "[profile eb-cli]" > ${AWS_CONFIG_FILE}
+  echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> ${AWS_CONFIG_FILE}
+  echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> ${AWS_CONFIG_FILE}
+  echo "" >> ${AWS_CONFIG_FILE}
+  echo "[default]" >> ${AWS_CONFIG_FILE}
+  echo "output = json" >> ${AWS_CONFIG_FILE}
+  echo "region = ${AWS_DEFAULT_REGION}" >> ${AWS_CONFIG_FILE}
+  chmod 600 /root/.aws/config
 fi
 
 export export PATH=$PATH:/root/.local/bin
 
 exec "$@"
+
 
